ec83dae31bef54fee5565a8e3b1cd1d72319e8f0
Albert Minnie <albert.minnie@gmail.com>
2017-04-21 15:30:06 +0200

    Example of passing data back to template from view callable

--- wikipedia_toc_scraper/views.py
+++ wikipedia_toc_scraper/views.py
@@ -19,7 +19,7 @@ from pyramid.view import view_config
 
 @view_config(route_name='toc', renderer='templates/toc.jinja2')
 def toc_view(request):
-    return {}
+    return {'foo': 'bar'}
 
 @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
 def my_view(request):
